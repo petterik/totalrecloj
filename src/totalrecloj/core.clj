5b92565b63e2d997fac560dc1b7c6c91ead1119b
Petter Eriksson <periksson@splunk.com>
2013-04-02 14:19:51 -0700

    Implemented half the email-token logic

--- src/totalrecloj/core.clj
+++ src/totalrecloj/core.clj
@@ -1,4 +1,17 @@
 (ns totalrecloj.core)
 
 
+(defn generate-token-link 
+  "create GET link that hits the endpoint at the host with the token as data"
+  [{:keys [token host endpoint]}]
+  (str host endpoint "?token=" (java.net.URLEncoder/encode token)))
 
+(defn handle-postal-response [m]
+  {:email-request (select-keys m [:code :message])})
+
+(defn generate-token [] (java.util.UUID/randomUUID))
+
+(defn email-token! [{:keys [user password endpoint token-handler email-fn]}]
+  (do 
+    (.assoc-with-token token-handler (str (generate-token)) user password)
+    nil))
--- test/totalrecloj/core_test.clj
+++ test/totalrecloj/core_test.clj
@@ -2,4 +2,49 @@
   (:use midje.sweet
         totalrecloj.core))
 
-(fact (+ 1 1) => 2)
+(facts
+  "generate-token-link"
+  (fact (generate-token-link {:token "ma-token"
+                              :host "http://www.example.com"
+                              :endpoint "/endpoint"})
+        => "http://www.example.com/endpoint?token=ma-token")  
+  (fact "token is url encoded"
+        (generate-token-link {:token "with-special-chars-?/"
+                              :host "http://www.example.com"
+                              :endpoint "/endpoint"})
+        => (str "http://www.example.com/endpoint?token=" 
+                (java.net.URLEncoder/encode "with-special-chars-?/"))))
+
+(facts
+  "handle-postal-response"
+  (fact "keep message and code"
+        (handle-postal-response {:code 0
+                                 :error :SUCCESS
+                                 :message "yay"}) => {:email-request {:code 0
+                                                                      :message "yay"}}))
+
+(fact "generate-token is a java.util.UUID for uniqueness"
+      (class (generate-token)) => java.util.UUID)
+
+(defprotocol TokenHandler
+  (assoc-with-token [this token user-id password] "assoc's token with user and pass")
+  (get-with-token [this token] "get's user and pass with token")
+  (dissoc-token [this token] "dissoc's token from user and password"))
+
+(facts 
+  "sending token email"
+  (fact "assoc's token with user and pass"
+        (let [mem (atom {})
+              assocer (reify TokenHandler 
+                        (assoc-with-token [this token user pass]
+                          (reset! mem {:token token :user user :password pass})))]
+          (email-token! {:user "user" 
+                         :password "pass"
+                         :endpoint "/endpoint"
+                         :token-handler assocer
+                         :email-fn (fn [_] nil)})
+          (:user @mem) => "user"
+          (:password @mem) => "pass"
+          (-> (:token @mem) frequencies (get \-)) => 4))
+  (future-fact "sends the right things to the email fn"))
+
